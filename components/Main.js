f13492d5345c186b46a99ef8a0a6c9d2ffdb7837
namishk <namishkhandelwal1@gmail.com>
2022-05-22 09:30:14 +0530

    api

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,8 @@
-import {Text} from 'react-native';
+import react, {useState} from "react";
+import { View, Text, Button, TextInput, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Main({navigation}){
     return(<>
-    <Text>mainpage</Text>
+    <Text>main contents of application</Text>
     </>);
 }
--- components/screens/LoginPage.js
+++ components/screens/LoginPage.js
@@ -1,44 +1,65 @@
-import react, {useState} from "react";
+import react, { useState } from "react";
 import { View, Text, Button, TextInput, StyleSheet, TouchableOpacity } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function LoginPage({navigation}){
+export default function LoginPage({ navigation }) {
 
-    const [username, setUsername] = useState('');
+    const [email, setUserEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const loginBtn = () => {
-        console.log(username, password);
-        navigation.navigate('Main')
+    const loginBtn = async () => {
+        console.log(email, password);
+        // navigation.navigate('Main');
 
+        const uri = 'http://localhost:3000/register'
+
+        const data = JSON.stringify({email, password})
+        console.log(data)
+
+        var myHeaders = new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        });
+        
+        var requestOptions = {
+            method: 'POST',
+            mode: 'cors',
+            Headers: myHeaders,
+            body: data,
+        };
+        
+        fetch(uri, requestOptions)
+            .then(response => response.text().then(console.log(response)))
+            .then(result => console.log(result, "okiii"))
+            .catch(error => console.log('error', error));
     };
 
-    return(
+    return (
         <View style={styles.container}>
-            <Text style={styles.field}>Username
-            <TextInput style={styles.fieldInput} 
-                onChange={(e) =>  setUsername(e.target.value)}
-                placeholder={"Username"}
-            />
+            <Text style={styles.field}>UserEmail
+                <TextInput style={styles.fieldInput}
+                    onChange={(e) => setUserEmail(e.target.value)}
+                    placeholder={"UserEmail"}
+                />
             </Text>
 
             <Text style={styles.field}>Password
-            <TextInput style={styles.fieldInput} 
-                onChange={(e) =>  setPassword(e.target.value)}
-                placeholder={"Password"}
-                secureTextEntry={true}
-                
-            />
+                <TextInput style={styles.fieldInput}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder={"Password"}
+                    secureTextEntry={true}
+
+                />
             </Text>
 
 
 
             <TouchableOpacity style={styles.button}
-                onPress={() => {loginBtn()}}
+                onPress={() => { loginBtn() }}
             >
                 <Text>Login</Text>
             </TouchableOpacity>
-            
+
         </View>
     );
 }
@@ -54,10 +75,10 @@ const styles = StyleSheet.create({
         marginVertical: "1rem"
     },
     fieldInput: {
-       marginLeft: "1rem",
-       borderBottomWidth: 1,
+        marginLeft: "1rem",
+        borderBottomWidth: 1,
     },
-    button:{
+    button: {
         backgroundColor: "rgb(0, 100, 255)",
         paddingHorizontal: "1rem",
         paddingVertical: "0.5rem",
